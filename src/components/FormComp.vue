ba3dafe08c86debea28322ff1e173a3f87256aeb
Andrey Zinoviev <sttrog_810@mail.ru>
2022-07-13 18:44:26 +0300

    made position sticky for form element

--- src/components/FormComp.vue
+++ src/components/FormComp.vue
@@ -115,6 +115,8 @@
         box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
         border-radius: 4px;
         margin: 0 16px 0 0;
+        position: sticky;
+        top: 24px;
     }
     div {
         display: flex;
@@ -204,6 +206,11 @@
     @media screen and (max-width: 1023px) {
         form {
             margin: 0 0 16px 0;
+        }
+    }
+    @media screen and (max-width: 767px) {
+        form {
+            position: static;
         }
     }
 </style>
